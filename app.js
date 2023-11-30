f7a87dc425415aeeed24f770db170af5d2b9fbb3
Gulaghaa <gulagaabdul123@gmail.com>
2023-11-30 17:23:56 +0400

    product page

--- app.js
+++ app.js
@@ -2,9 +2,11 @@ const productsPerPage = 10;
 let currentPage = 1;
 let totalProducts = 0;
 
-const fetchData = async () => {
+totalAPIurl = 'https://dummyjson.com/products?limit=100';
+
+const fetchData = async (url) => {
     try {
-        const response = await axios.get("https://dummyjson.com/products?limit=100");
+        const response = await axios.get(url);
         const products = response.data.products;
         totalProducts = products.length;
         displayProducts(products);
@@ -15,9 +17,21 @@ const fetchData = async () => {
     }
 };
 
+const fetchDataById = async (productId) => {
+    const url = `https://dummyjson.com/products/${productId}`;
+    try {
+        const response = await axios.get(url);
+        const product = response.data; // Assuming the detailed product information is in the response
+        displayProductDetails(product);
+        console.log('Product details:', product);
+    } catch (error) {
+        handleError(error);
+    }
+};
+
 const displayProducts = (products) => {
     const container = document.getElementById('productContainer');
-    container.innerHTML = ''; 
+    container.innerHTML = '';
 
     const startIndex = (currentPage - 1) * productsPerPage;
     const endIndex = startIndex + productsPerPage;
@@ -26,14 +40,56 @@ const displayProducts = (products) => {
         const product = products[i];
         if (product) {
             const productElement = document.createElement('div');
+            productElement.classList.add('productElement');
             productElement.innerHTML = `
                 <h3>${product.title}</h3>
                 <p>Price: ${product.price}</p>
+                <button class="details-button" data-product-id="${product.id}">View Details</button>
                 <hr>
             `;
             container.appendChild(productElement);
         }
     }
+
+    // Add event listeners to the details buttons
+    const detailsButtons = document.querySelectorAll('.details-button');
+    detailsButtons.forEach(button => {
+        button.addEventListener('click', (event) => {
+            const productId = event.target.dataset.productId;
+            fetchDataById(productId);
+        });
+    });
+};
+
+const displayProductDetails = (product) => {
+    const productContainer = document.getElementById('productContainer');
+    const detailsContainer = document.getElementById('productDetails');
+    const paginationContainer = document.getElementById('pagination');
+
+    // Hide product list and pagination, and show details
+    productContainer.style.display = 'none';
+    paginationContainer.style.display = 'none';
+    detailsContainer.style.display = 'block';
+
+    detailsContainer.innerHTML = `
+        <h2>${product.title}</h2>
+        <p>Price: ${product.price}</p>
+        <p>Description: ${product.description}</p>
+        <button onclick="clearProductDetails()">Close Details</button>
+    `;
+};
+
+const clearProductDetails = () => {
+    const productContainer = document.getElementById('productContainer');
+    const detailsContainer = document.getElementById('productDetails');
+    const paginationContainer = document.getElementById('pagination');
+
+    // Show product list and pagination, hide details
+    productContainer.style.display = 'block';
+    paginationContainer.style.display = 'block';
+    detailsContainer.style.display = 'none';
+
+    detailsContainer.innerHTML = ''; // Clear the details container
 };
 
 const renderPagination = () => {
@@ -57,20 +113,20 @@ const renderPagination = () => {
 const changePage = (newPage) => {
     if (newPage >= 1 && newPage <= Math.ceil(totalProducts / productsPerPage)) {
         currentPage = newPage;
-        fetchData();
+        fetchData(totalAPIurl);
     }
 };
 
 const prevPage = () => {
     if (currentPage > 1) {
         currentPage--;
-        fetchData();
+        fetchData(totalAPIurl);
     }
 };
 
 const nextPage = () => {
     currentPage++;
-    fetchData();
+    fetchData(totalAPIurl);
 };
 
 const handleError = (error) => {
@@ -85,4 +141,4 @@ const handleError = (error) => {
 };
 
 // Initial data fetch
-fetchData();
+fetchData(totalAPIurl);
